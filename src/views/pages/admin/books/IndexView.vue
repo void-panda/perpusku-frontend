e60110e4e097a05cdd206ca1789197c4f0430540
Void Panda <hamzankechil@gmail.com>
2025-02-13 19:39:53 +0800

    Initial w/-out @tanstack/vue-table

--- src/views/pages/admin/books/IndexView.vue
+++ src/views/pages/admin/books/IndexView.vue
@@ -1,68 +1,14 @@
 <script lang="ts" setup>
-import type { Payment } from '@/types/payment';
-import { columns } from '@/views/components/payments/columns';
-import DataTable from '@/views/components/payments/data-table.vue';
-import { Breadcrumb, BreadcrumbEllipsis, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/views/components/ui/breadcrumb';
 import MainLayout from '@/views/layouts/MainLayout.vue';
-import { onMounted, ref } from 'vue';
 
-const data = ref<Payment[]>([])
-
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: '728ed52f',
-      amount: 100,
-      status: 'pending',
-      email: 'm@example.com',
-    },
-    // ...
-  ]
-}
-
-onMounted(async () => {
-  data.value = await getData()
-})
 </script>
 
 <template>
   <MainLayout>
     <template #header>
-      <div class="space-y-1">
-        <h1 class="font-semibold text-lg md:text-2xl">Books</h1>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink as-child>
-                <a href="/">
-                  Home
-                </a>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbEllipsis />
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink as-child>
-                <a href="/docs/components/accordion.html">
-                  Components
-                </a>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
+      <h1 class="font-semibold text-lg md:text-2xl">Books</h1>
     </template>
 
-
-    <DataTable :columns="columns" :data="data" />
 
   </MainLayout>
 </template>
